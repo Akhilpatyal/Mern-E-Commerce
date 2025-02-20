12d5ab5618d0663e49ca814f4732b07a3b4f51a5
Akhil <84496553+Akhilpatyal@users.noreply.github.com>
2025-02-20 09:49:21 +0000

    comit remove function

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,26 +9,27 @@ import Orders from './pages/Orders'
 import { ToastContainer } from 'react-toastify';
 
 
-export const backendURL=import.meta.env.VITE_BACKEND_URL
+export const backendURL = import.meta.env.VITE_BACKEND_URL
+export const currency = "$";
 const App = () => {
 
-  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):"");
-  useEffect(()=>{
-    localStorage.setItem('token',token)
-  },[token])
+  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
+  useEffect(() => {
+    localStorage.setItem('token', token)
+  }, [token])
   return (
     <div className='bg-gray-50 min-h-screen'>
-      <ToastContainer/>
+      <ToastContainer />
       {token === "" ? <Login setToken={setToken} /> : <>
-        <Navbar setToken={setToken}/>
+        <Navbar setToken={setToken} />
         <hr />
         <div className="flex w-full">
           <Sidebar />
           <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
             <Routes>
-              <Route path='/add' element={<Add Token={setToken}/>} />
-              <Route path='/list' element={<List Token={setToken}/>} />
-              <Route path='/Orders' element={<Orders Token={setToken}/>} />
+              <Route path='/add' element={<Add token={token} />} />
+              <Route path='/list' element={<List token={token} />} />
+              <Route path='/Orders' element={<Orders token={token} />} />
             </Routes>
           </div>
         </div>
--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,15 +1,43 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { backendURL } from '../App';
+import { backendURL, currency } from '../App';
+import { toast } from 'react-toastify';
 
-const List = () => {
+const List = ({ token }) => {
   const [list, setList] = useState([]);
   const fetchList = async () => {
     try {
       const response = await axios.get(backendURL + '/api/product/list')
-      console.log(response.data);
+      if (response.data.success) {
+        // console.log(response.data);
+        setList(response.data?.product || []);
+
+      } else {
+        toast.error(response.data.message);
+      }
 
     } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+
+    }
+  }
+
+  const removeProduct = async (id) => {
+    try {
+      const response = await axios.post(backendURL + '/api/product/remove',{id} ,{ headers: { token } })
+      if (response.data.success) {
+        // console.log(response.data);
+        toast.success(response.data.message)
+        await fetchList();
+
+      } else {
+        toast.error(response.data.message);
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
 
     }
   }
@@ -18,7 +46,27 @@ const List = () => {
   }, [])
   return (
     <div>
-
+      <p>All Proucts List</p>
+      {/* List Table Title */}
+      <div className="hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm font-semibold">
+        <b>Images</b>
+        <b>Name</b>
+        <b>Category</b>
+        <b>Price</b>
+        <b>Action</b>
+      </div>
+      {/* product List */}
+      {
+        list.map((item, index) => (
+          <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 gap-2 px-2 border bg-gray-100 text-sm font-semibold' key={index}>
+            <img className='w-12' src={item.image[0]} alt="" />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>{currency}{item.price}</p>
+            <p className='text-right text-lg cursor-pointer md:text-center' onClick={() => removeProduct(item._id)}>X</p>
+          </div>
+        ))
+      }
     </div>
   )
 }
